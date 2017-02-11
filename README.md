d0063401bd7c115225e626f380ee3a5bd65af320
Stuart Pineo <svpineo@gmail.com>
2017-02-11 13:42:13 -0500

    Photo library UI testing (refs #E222)

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ Unit and UI Testing in Swift with XCTest framework and Accessibility. Requiremen
 
 Dependencies: XCTest/XCTest.h (The XCTest file need to be added as a compile target to Tests under 'BuildPhases' for this to build)
 
-Other Possible Configurations:
-
-Under 'General' settings, set the host for the target applications (i.e., *Tests and *UITests) to 'None'.
+Requirements:
+Jenkins server running during the test.
--- RGButterflyUITests.swift
+++ RGButterflyUITests.swift
@@ -8,29 +8,62 @@
 
 import XCTest
 
+
 class RGButterflyUITests: XCTestCase {
-        
+    
     override func setUp() {
         super.setUp()
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        //
         continueAfterFailure = false
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        // Launch new instance of the App
+        //
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // this is the line responsible for the race condition
+        //NSRunLoop.mainRunLoop().runUntilDate(NSDate())
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        //
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Main ViewController UI Testing
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // (1) Photo Library
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Select an image from the Photo Library
+    //
+    func testPhotoLibrarySelection() {
+        let app = XCUIApplication()
+        app.navigationBars["RGButterfly"].buttons["photo 2"].tap()
+        app.alerts["Photo Selection"].buttons["My Photo Library"].tap()
+        app.tables.buttons["Moments"].tap()
+        app.collectionViews["PhotosGridView"].cells["Photo, Landscape, March 12, 2011, 7:17 PM"].tap()
     }
     
+    // Cancel button at the 'Photo Selection' popup
+    //
+    func testPhotoSelectionCancel() {
+        let app = XCUIApplication()
+        app.navigationBars["RGButterfly"].buttons["photo 2"].tap()
+        app.alerts["Photo Selection"].buttons["Cancel"].tap()
+    }
+
+    // First Cancel button after the Photo Library load
+    //
+    func testPhotoLibrarySelectionCancel() {
+        let app = XCUIApplication()
+        app.navigationBars["RGButterfly"].buttons["photo 2"].tap()
+        app.alerts["Photo Selection"].buttons["My Photo Library"].tap()
+        app.navigationBars["Photos"].buttons["Cancel"].tap()
+    }
 }
