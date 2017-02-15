f8d388fa0f2fc7276f31d27adf230f00d4f2a274
Stuart Pineo <svpineo@gmail.com>
2017-02-15 09:15:46 -0500

    Unit testing: Create the 'testInitViewController' function defining storyboard globally (refs #E221)

--- RGButterflyTests.swift
+++ RGButterflyTests.swift
@@ -10,17 +10,11 @@ import XCTest
 
 
 class RGButterflyTests: XCTestCase {
+  
+    let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        var initVC: InitViewController = InitViewController()
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main);
-        initVC = storyboard.instantiateInitialViewController() as! InitViewController
-        if initVC.accessibilityActivate() == true {
-            print("Accessibility Activate")
-        }
     }
     
     override func tearDown() {
@@ -28,9 +22,14 @@ class RGButterflyTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitViewController() {
+        var initVC: InitViewController = InitViewController()
+        initVC = storyboard.instantiateInitialViewController() as! InitViewController
+        
+        var view: UIView = UIView()
+        view = initVC.view
+        
+        XCTAssertNotNil(view)
     }
     
     
